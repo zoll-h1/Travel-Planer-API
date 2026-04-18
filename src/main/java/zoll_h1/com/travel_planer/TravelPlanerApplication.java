60273f09de3fc8293a68c3e40d6a571a10b10c8d
zoll-h1 <nurbekerkibaev9@gmail.com>
2026-04-18 16:22:29 +0600

    Feat: Travel-Planer API

---
+++ src/main/java/zoll_h1/com/travel_planer/TravelPlanerApplication.java
@@ -0,0 +1,13 @@
+package zoll_h1.com.travel_planer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPlanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zoll_h1/com/travel_planer/TravelPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package zoll_h1.com.travel_planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
